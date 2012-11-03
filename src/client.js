c6444f28015dc53dfcec1829fbb9a3fa8e48b5d4
Jake Schwartz <jacob_schwartz@apple.com>
2012-11-02 21:54:12 -0700

    Client generates canvas board

--- src/client.js
+++ src/client.js
@@ -43,11 +43,17 @@ var Test = Class(function() {
     },
 
     connected: function() {
-        this.log('Connection established');
+        console.log('Connection established');
     },
 
     message: function(type, tick, data) {
-        //this.log('Message received:', type, data);
+        this.log('Message received:', type, data);
+        if (data[0] && data[0] == 'board') {
+            if (data[1]) {
+                this._updateBoard(data[1]);
+                this._initGame();
+            }
+        }
     },
 
     syncedMessage: function(type, tick, data) {
@@ -56,6 +62,67 @@ var Test = Class(function() {
 
     closed: function(byRemote, errorCode) {
         this.log('Connection closed:', byRemote, errorCode);
+    },
+
+
+    _board: [],
+    _boardWidth: 0,
+    _boardHeight: 0,
+
+    _canasHeight: 0,
+    _canvasWidth: 0,
+
+    _updateBoard: function(boardString) {
+        this._board = this._parseBoardString(boardString);
+        this._drawBoard();
+        this.log(this._board);
+    },
+
+    _parseBoardString: function(boardString) {
+        var arr = boardString.split(' ');
+        this._boardWidth = parseInt(arr.shift());
+        this._boardHeight = parseInt(arr.shift());
+        var board = [];
+
+        arr = arr.shift().split('');
+        for (var y=0; y < this._boardHeight; y++) {
+            board[y] = arr.splice(0, this._boardWidth);
+        }
+
+        return board;
+    },
+
+    _drawBoard: function() {
+        this._canvasHeight = 400;
+        this._canvasWidth = 400;
+        var canvas = $('<canvas id="board" width="'+this._canvasWidth+'" height="'+this._canvasHeight+'"></canvas>')[0];
+        var context = canvas.getContext('2d');
+
+        var boxWidth = this._canvasWidth / this._boardWidth;
+        var boxHeight = this._canvasHeight / this._boardHeight;
+        var fontSize = 64;
+        for (var y=0; y < this._boardHeight; y++) {
+            for (var x=0; x < this._boardWidth; x++) {
+                var boxX = x * boxWidth + 8.5;
+                var boxY = y * boxHeight + 8.5;
+                context.fillStyle = 'blue';
+                context.strokeStyle = 'black';
+                context.lineWidth = 2;
+                context.fillRect(boxX, boxY, boxWidth - 14.5, boxHeight - 14.5);
+                context.strokeRect(boxX, boxY, boxWidth - 14.5, boxHeight - 14.5);
+
+                context.font = 'bold '+ fontSize +'px sans-serif';
+                context.lineWidth = 1;
+                context.fillStyle = 'black';
+                context.fillText(this._board[y][x], boxX + 35, boxY + 64);
+            }
+        }
+
+        document.body.appendChild(canvas);
+    },
+
+    _initGame: function() {
+
     }
 
 });
